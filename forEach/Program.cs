43b7d416a5ea64e9b1a008c23e04a638c29cb65f
Hussen Fakhrulddin <hf556482@gmail.com>
2016-07-25 00:53:56 -0700

    init

---
+++ forEach/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace forEach
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Testing Tokens by breaking the string into tokens:
+            Tokens f = new Tokens("Hello is it-me you are looking for.", new char[] { ' ', '-' });
+
+            foreach (string item in f)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
